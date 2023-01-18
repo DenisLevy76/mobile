0b92f4cfa006019190b102f3d46cec57986d44a0
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2023-01-18 15:15:48 -0300

    Add: header component

---
+++ src/components/HeaderComponent/index.tsx
@@ -0,0 +1,21 @@
+import { View, TouchableOpacity, Text } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import colors from 'tailwindcss/colors'
+
+import Logo from '../../assets/logo.svg'
+
+export const HeaderComponent: React.FC = () => {
+  return (
+    <View className='w-full flex-row align-center justify-between px-8 py-20'>
+      <Logo />
+      <TouchableOpacity className='flex-row h-11 px-4 items-center border border-violet-500 rounded-lg'>
+        <Feather
+          name='plus'
+          color={colors.violet[500]}
+          size={20}
+        />
+        <Text className='text-white font-semibold'>Novo</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native'
+import { HeaderComponent } from '../../components/HeaderComponent'
 
 export const Home: React.FC = () => {
   return (
-    <View>
-      <Text>HomePage</Text>
+    <View className='bg-main-bg flex-1'>
+      <HeaderComponent />
     </View>
   )
 }
